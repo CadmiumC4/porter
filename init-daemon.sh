b595200b3312f44816aa3e4027b1b2636533b232
Yiğit Cemal Öztürk <69117901+CadmiumC4@users.noreply.github.com>
2021-05-24 19:02:09 +0300

    Create init-daemon.sh

---
+++ init-daemon.sh
@@ -0,0 +1,21 @@
+
+for dir in $(ls)
+do 
+    pushd $dir
+    git init
+    echo "initial commit"| git commit
+    popd
+done
+which ssh-keygen
+ssh-keygen -o
+sudo adduser git
+su git
+cd
+
+
+mkdir .ssh && chmod 700 .ssh
+touch .ssh/authorized_keys && chmod 600 .ssh/authorized_keys
+cat /home/cadmium/.ssh/id_rsa.pub >> .ssh/authorized_keys
+cd ~/mnt/c/Users/User/source/repos
+git daemon --reuseaddr --base-path=. .
+
